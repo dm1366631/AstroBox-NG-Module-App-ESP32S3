3b81b7eaafe77a34b46a6921d4bf1f66b618a3b6
dm1366631 <dm1366631@users.noreply.github.com>
2026-08-24 16:50:52 +0800

    Fix UncheckedAdvice to be repr(i32) enum for as-cast

--- .github/stubs/advice.rs
+++ .github/stubs/advice.rs
@@ -8,11 +8,33 @@ pub enum Advice {
     DontNeed = 4,
 }
 
-#[repr(transparent)]
+#[repr(i32)]
 #[derive(Clone, Copy, Debug, Eq, PartialEq, Hash)]
-pub struct UncheckedAdvice(pub i32);
+pub enum UncheckedAdvice {
+    Normal = 0,
+    Random = 1,
+    Sequential = 2,
+    WillNeed = 3,
+    DontNeed = 4,
+}
 
 impl UncheckedAdvice {
-    pub const fn new(val: i32) -> Self { Self(val) }
-    pub const fn from_advice(advice: Advice) -> Self { Self(advice as i32) }
+    pub fn new(val: i32) -> Self {
+        match val {
+            0 => UncheckedAdvice::Normal,
+            1 => UncheckedAdvice::Random,
+            2 => UncheckedAdvice::Sequential,
+            3 => UncheckedAdvice::WillNeed,
+            _ => UncheckedAdvice::DontNeed,
+        }
+    }
+    pub fn from_advice(advice: Advice) -> Self {
+        match advice {
+            Advice::Normal => UncheckedAdvice::Normal,
+            Advice::Random => UncheckedAdvice::Random,
+            Advice::Sequential => UncheckedAdvice::Sequential,
+            Advice::WillNeed => UncheckedAdvice::WillNeed,
+            Advice::DontNeed => UncheckedAdvice::DontNeed,
+        }
+    }
 }
